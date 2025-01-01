95afd8bbfcd6e7e65537dda899dad7e4cf49d7a5
DavigJ <david.j.jung@gmail.com>
2025-01-01 09:56:29 +0400

    mixin optimization

--- src/main/java/com/davigj/frame_changer/core/mixin/BaseFireBlockMixin.java
+++ src/main/java/com/davigj/frame_changer/core/mixin/BaseFireBlockMixin.java
@@ -2,46 +2,27 @@ package com.davigj.frame_changer.core.mixin;
 
 import com.davigj.frame_changer.core.FCConfig;
 import com.davigj.frame_changer.core.other.FCBlockTags;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.AirBlock;
 import net.minecraft.world.level.block.BaseFireBlock;
-import net.minecraft.world.level.block.NetherPortalBlock;
-import net.minecraft.world.level.portal.PortalShape;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import static net.minecraft.world.level.block.BaseFireBlock.inPortalDimension;
 
 @Mixin(BaseFireBlock.class)
 public class BaseFireBlockMixin {
-    @Inject(method = "isPortal", at = @At("HEAD"), cancellable = true)
-    private static void cooking(Level level, BlockPos pos, Direction dir, CallbackInfoReturnable<Boolean> cir) {
-        // I wonder if I should work on making head-inject mixins that don't just copy and paste the entire method :sob:
+    @WrapOperation(method = "isPortal", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;isPortalFrame(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Z"))
+    private static boolean isPortalFrame(BlockState instance, BlockGetter blockGetter, BlockPos pos, Operation<Boolean> original, Level level, BlockPos blockPos, @Local BlockPos.MutableBlockPos mutable,
+                             @Local boolean flag, @Local(ordinal = 1) Direction direction) {
         if (FCConfig.COMMON.portalFrameTag.get()) {
-            if (!inPortalDimension(level)) {
-                cir.setReturnValue(false);
-            } else {
-                BlockPos.MutableBlockPos blockpos$mutableblockpos = pos.mutable();
-                boolean flag = false;
-                for(Direction direction : Direction.values()) {
-                    if (level.getBlockState(blockpos$mutableblockpos.set(pos).move(direction)).is(FCBlockTags.PORTAL_FRAMES)) {
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (!flag) {
-                    cir.setReturnValue(false);
-                } else {
-                    Direction.Axis direction$axis = dir.getAxis().isHorizontal() ? dir.getCounterClockWise().getAxis() : Direction.Plane.HORIZONTAL.getRandomAxis(level.random);
-                    cir.setReturnValue(PortalShape.findEmptyPortalShape(level, pos, direction$axis).isPresent());
-                }
-            }
+            return level.getBlockState(mutable.set(pos).move(direction)).is(FCBlockTags.PORTAL_FRAMES);
+        } else {
+            return original.call(instance, blockGetter, pos);
         }
     }
-
 }
--- src/main/java/com/davigj/frame_changer/core/mixin/PortalForcerMixin.java
+++ src/main/java/com/davigj/frame_changer/core/mixin/PortalForcerMixin.java
@@ -2,18 +2,15 @@ package com.davigj.frame_changer.core.mixin;
 
 import com.davigj.frame_changer.core.FCConfig;
 import com.davigj.frame_changer.core.FrameChanger;
-import net.minecraft.BlockUtil;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.Mth;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.NetherPortalBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.border.WorldBorder;
-import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.portal.PortalForcer;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -21,15 +18,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Optional;
 import java.util.function.Supplier;
 
 @Mixin(PortalForcer.class)
 public class PortalForcerMixin {
-    // Bad code! Bad code !! :sob:
+    // Bad code, but the best I can make it
     @Unique
     private final static Logger framechanger$LOGGER = LogManager.getLogger(FrameChanger.MOD_ID);
 
@@ -42,12 +36,22 @@ public class PortalForcerMixin {
         this.level = level;
     }
 
-
-    @Inject(method = "createPortal", at = @At("HEAD"), cancellable = true)
-    private void onCreatePortal(BlockPos p_77667_, Direction.Axis p_77668_, CallbackInfoReturnable<Optional<BlockUtil.FoundRectangle>> cir) {
+    @WrapOperation(method = "createPortal", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;setBlockAndUpdate(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Z"))
+    private boolean customExit(ServerLevel instance, BlockPos pos, BlockState state, Operation<Boolean> original, @Local(ordinal = 3) int k3) {
+        BlockState blockState = state;
         if (FCConfig.COMMON.customExitPortals.get()) {
-            cir.setReturnValue(this.framechanger$createCustomPortal(p_77667_, p_77668_));
+            blockState = k3 < 0 ? framechanger$getCompatBlock(FCConfig.COMMON.exitPortalFrame.get()).get().defaultBlockState() : Blocks.AIR.defaultBlockState();
         }
+        return original.call(instance, pos, blockState);
+    }
+
+    @WrapOperation(method = "createPortal", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z", ordinal = 0))
+    private boolean customExitPt2(ServerLevel instance, BlockPos pos, BlockState state, int i, Operation<Boolean> original) {
+        BlockState blockState = state;
+        if (FCConfig.COMMON.customExitPortals.get()) {
+            blockState = framechanger$getCompatBlock(FCConfig.COMMON.exitPortalFrame.get()).get().defaultBlockState();
+        }
+        return original.call(instance, pos, blockState, i);
     }
 
     @Unique
@@ -55,11 +59,10 @@ public class PortalForcerMixin {
         String[] parts = fullId.split(":");
         if (parts.length != 2) {
             framechanger$LOGGER.warn("Improperly formatted config for generated portal frames. String should be formatted 'modid:blockID'. Defaulting to minecraft:obsidian");
+            return () -> Blocks.OBSIDIAN;
         }
-
         String modid = parts[0];
         String blockID = parts[1];
-
         if (!ModList.get().isLoaded(modid) && modid != null) {
             framechanger$LOGGER.warn("Mod '" + modid + "' not loaded, invalid configured exit portal frame blockID. String should be formatted 'modID:blockID'. Defaulting to minecraft:obsidian");
             return () -> Blocks.OBSIDIAN;
@@ -73,101 +76,4 @@ public class PortalForcerMixin {
         return (ModList.get().isLoaded(modid) ? () -> ForgeRegistries.BLOCKS.getValue(block) : () -> null);
     }
 
-    @Unique
-    private Optional<BlockUtil.FoundRectangle> framechanger$createCustomPortal(BlockPos pos, Direction.Axis axis) {
-        PortalForcer forcer = (PortalForcer) (Object) this;
-        Direction direction = Direction.get(Direction.AxisDirection.POSITIVE, axis);
-        double d0 = -1.0D;
-        BlockPos blockpos = null;
-        double d1 = -1.0D;
-        BlockPos blockpos1 = null;
-        WorldBorder worldborder = this.level.getWorldBorder();
-        int i = Math.min(this.level.getMaxBuildHeight(), this.level.getMinBuildHeight() + this.level.getLogicalHeight()) - 1;
-        BlockPos.MutableBlockPos blockpos$mutableblockpos = pos.mutable();
-
-        for(BlockPos.MutableBlockPos blockpos$mutableblockpos1 : BlockPos.spiralAround(pos, 16, Direction.EAST, Direction.SOUTH)) {
-            int j = Math.min(i, this.level.getHeight(Heightmap.Types.MOTION_BLOCKING, blockpos$mutableblockpos1.getX(), blockpos$mutableblockpos1.getZ()));
-            int k = 1;
-            if (worldborder.isWithinBounds(blockpos$mutableblockpos1) && worldborder.isWithinBounds(blockpos$mutableblockpos1.move(direction, 1))) {
-                blockpos$mutableblockpos1.move(direction.getOpposite(), 1);
-
-                for(int l = j; l >= this.level.getMinBuildHeight(); --l) {
-                    blockpos$mutableblockpos1.setY(l);
-                    if (this.level.isEmptyBlock(blockpos$mutableblockpos1)) {
-                        int i1;
-                        for(i1 = l; l > this.level.getMinBuildHeight() && this.level.isEmptyBlock(blockpos$mutableblockpos1.move(Direction.DOWN)); --l) {
-                        }
-
-                        if (l + 4 <= i) {
-                            int j1 = i1 - l;
-                            if (j1 <= 0 || j1 >= 3) {
-                                blockpos$mutableblockpos1.setY(l);
-                                if (forcer.canHostFrame(blockpos$mutableblockpos1, blockpos$mutableblockpos, direction, 0)) {
-                                    double d2 = pos.distSqr(blockpos$mutableblockpos1);
-                                    if (forcer.canHostFrame(blockpos$mutableblockpos1, blockpos$mutableblockpos, direction, -1) && forcer.canHostFrame(blockpos$mutableblockpos1, blockpos$mutableblockpos, direction, 1) && (d0 == -1.0D || d0 > d2)) {
-                                        d0 = d2;
-                                        blockpos = blockpos$mutableblockpos1.immutable();
-                                    }
-
-                                    if (d0 == -1.0D && (d1 == -1.0D || d1 > d2)) {
-                                        d1 = d2;
-                                        blockpos1 = blockpos$mutableblockpos1.immutable();
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        if (d0 == -1.0D && d1 != -1.0D) {
-            blockpos = blockpos1;
-            d0 = d1;
-        }
-
-        if (d0 == -1.0D) {
-            int k1 = Math.max(this.level.getMinBuildHeight() - -1, 70);
-            int i2 = i - 9;
-            if (i2 < k1) {
-                return Optional.empty();
-            }
-
-            blockpos = (new BlockPos(pos.getX(), Mth.clamp(pos.getY(), k1, i2), pos.getZ())).immutable();
-            Direction direction1 = direction.getClockWise();
-            if (!worldborder.isWithinBounds(blockpos)) {
-                return Optional.empty();
-            }
-
-            for(int i3 = -1; i3 < 2; ++i3) {
-                for(int j3 = 0; j3 < 2; ++j3) {
-                    for(int k3 = -1; k3 < 3; ++k3) {
-                        BlockState blockstate1 = k3 < 0 ? framechanger$getCompatBlock(FCConfig.COMMON.exitPortalFrame.get()).get().defaultBlockState() : Blocks.AIR.defaultBlockState();
-                        blockpos$mutableblockpos.setWithOffset(blockpos, j3 * direction.getStepX() + i3 * direction1.getStepX(), k3, j3 * direction.getStepZ() + i3 * direction1.getStepZ());
-                        this.level.setBlockAndUpdate(blockpos$mutableblockpos, blockstate1);
-                    }
-                }
-            }
-        }
-
-        for(int l1 = -1; l1 < 3; ++l1) {
-            for(int j2 = -1; j2 < 4; ++j2) {
-                if (l1 == -1 || l1 == 2 || j2 == -1 || j2 == 3) {
-                    blockpos$mutableblockpos.setWithOffset(blockpos, l1 * direction.getStepX(), j2, l1 * direction.getStepZ());
-                    this.level.setBlock(blockpos$mutableblockpos, framechanger$getCompatBlock(FCConfig.COMMON.exitPortalFrame.get()).get().defaultBlockState(), 3);
-                }
-            }
-        }
-
-        BlockState blockstate = Blocks.NETHER_PORTAL.defaultBlockState().setValue(NetherPortalBlock.AXIS, axis);
-
-        for(int k2 = 0; k2 < 2; ++k2) {
-            for(int l2 = 0; l2 < 3; ++l2) {
-                blockpos$mutableblockpos.setWithOffset(blockpos, k2 * direction.getStepX(), l2, k2 * direction.getStepZ());
-                this.level.setBlock(blockpos$mutableblockpos, blockstate, 18);
-            }
-        }
-
-        return Optional.of(new BlockUtil.FoundRectangle(blockpos.immutable(), 2, 3));
-    }
 }
